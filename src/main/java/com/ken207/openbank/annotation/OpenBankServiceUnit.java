99c6d54d60fbd91ab478093804f9bdb874bd3e9e
ken207 <kute80@naver.com>
2019-11-28 00:15:59 +0900

    type annotation 주입방법 진행중

--- src/main/java/com/ken207/openbank/annotation/OpenBankServiceUnit.java
+++ src/main/java/com/ken207/openbank/annotation/OpenBankServiceUnit.java
@@ -2,17 +2,19 @@ package com.ken207.openbank.annotation;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 @Component
+@Aspect
 public class OpenBankServiceUnit {
 
     Logger logger = LoggerFactory.getLogger(OpenBankServiceUnit.class);
 
-    @Around("@Annotation(OpenBankService)")
+    @Around("@annotation(OpenBankService)")
     public Object createGlobalId(ProceedingJoinPoint joinPoint) throws Throwable {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
