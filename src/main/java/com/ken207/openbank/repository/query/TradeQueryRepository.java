63d1e534dfcb53ec97957eecf135e30b3d07fb97
geeshow <kute80@naver.com>
2019-12-27 23:27:04 +0900

    added TradeQueryRepository

---
+++ src/main/java/com/ken207/openbank/repository/query/TradeQueryRepository.java
@@ -0,0 +1,25 @@
+package com.ken207.openbank.repository.query;
+
+import com.ken207.openbank.domain.TradeEntity;
+import com.ken207.openbank.repository.TradeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class TradeQueryRepository {
+
+    private final EntityManager em;
+    private final TradeRepository tradeRepository;
+
+    public Map<String, Long> getDailyBalanceFrom(Long accountId, String lastInterestPayDate) {
+        List<TradeEntity> tradeListForInterest = tradeRepository.findByAccountIdAndTradeDateGreaterThanOrderBySrnoAsc(accountId, lastInterestPayDate);
+
+        return tradeListForInterest.stream().collect(Collectors.toMap(TradeEntity::getBzDate, TradeEntity::getBlncAfter));
+    }
+}
