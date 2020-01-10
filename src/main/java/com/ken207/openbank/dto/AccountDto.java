7949f3ba386e4ba0a552602e3105b995b05f6ebb
geeshow <kute80@naver.com>
2020-01-10 16:01:52 +0900

    Revert "renamed entity classes"
    This reverts commit 7038f8aa01822030d26222572cf1e32478e18ea1.

--- src/main/java/com/ken207/openbank/dto/AccountDto.java
+++ src/main/java/com/ken207/openbank/dto/AccountDto.java
@@ -1,5 +1,6 @@
 package com.ken207.openbank.dto;
 
+import com.ken207.openbank.domain.AccountEntity;
 import com.ken207.openbank.domain.enums.AccountStatusCode;
 import com.ken207.openbank.domain.enums.SubjectCode;
 import com.ken207.openbank.domain.enums.TaxationCode;
