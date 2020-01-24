9ded0d4b047e52b2c6c091cfd72f58310b2c4d3e
geeshow <kute80@naver.com>
2020-01-24 23:02:26 +0900

    전표 엔티티 추가 작업

---
+++ src/main/java/com/ken207/openbank/domain/Statement.java
@@ -0,0 +1,26 @@
+package com.ken207.openbank.domain;
+
+import com.ken207.openbank.domain.enums.InOutCd;
+import com.ken207.openbank.domain.enums.TradeCd;
+import com.ken207.openbank.domain.enums.YesNo;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDateTime;
+
+@Entity
+public class Statement extends BaseEntity<Statement> {
+
+    private LocalDateTime tradeDate;
+    private String tradeId;
+    private int subSrno;
+    private String accountSubjectCode;
+    private Long tradeAmount;
+
+    @Enumerated(EnumType.STRING)
+    private YesNo isCancel;
+
+    @Enumerated(EnumType.STRING)
+    private InOutCd inOutCd;
+}
---
+++ src/main/java/com/ken207/openbank/domain/enums/InOutCd.java
@@ -0,0 +1,5 @@
+package com.ken207.openbank.domain.enums;
+
+public enum InOutCd {
+    RECEVIED, PAID
+}
